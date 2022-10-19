bf498c90569cc60549e57730bd43ac4ae6d40840
YaroslavKSE <ikhomych@kse.org.ua>
2022-10-19 12:49:44 +0300

    Implemented "Encrypt function", added "GetIndex" function which finds index of element in array, added "Print" function which prints the array. Added "IsPunctuation" function which checks if element is a punctuation mark. Added usage.

--- Caesar Encryption Algorithm PPO/Program.cs
+++ Caesar Encryption Algorithm PPO/Program.cs
@@ -1,3 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
 
-Console.WriteLine("Hello, World!");
+char[] Encrypt(char[] text, int key)
+{
+    var encryptedText = new char[text.Length];
+    for (int i = 0; i < text.Length; i++)
+    {
+        var notCodedElement = char.Parse(text[i].ToString().ToUpper());
+        var indexOfNotCodedElement = GetIndex(alphabet, notCodedElement);
+        
+        if (IsPunctuation(text[i]))
+        {
+            encryptedText[i] = text[i];
+        }
+        else
+        {
+            if (GetIndex(alphabet, notCodedElement) + key  <= 25)
+            {
+                encryptedText[i] = alphabet[key + indexOfNotCodedElement];
+            }
+            else
+            {
+                encryptedText[i] = alphabet[(key + indexOfNotCodedElement) % 25];
+            }
+        }
+    }
+    
+    return encryptedText;
+}
+
+int GetIndex(char[] array, char element)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == element)
+        {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+bool IsPunctuation(char element)
+{
+    return element is ' ' or ',' or ';' or '-' or '.';
+}
+
+void Print(char[] array)
+{
+    foreach (var element in array)
+    {
+        Console.Write(element);
+    }
+    Console.WriteLine();
+}
+
+while (true)
+{
+    Console.WriteLine("Enter text:");
+    var userInput = Console.ReadLine()!.ToCharArray();
+    Console.WriteLine("Enter key:");
+    var key = Console.ReadLine();
+    var encrypted = Encrypt(userInput, int.Parse(key));
+    Print(encrypted);
+}
